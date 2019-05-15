e21012a87d3f146582a7036cc012286e1b112d06
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-05-15 12:21:51 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+class palindrome
+{
+	public static void main(String[]args)throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+			char ch[]=s.toCharArray();
+			int i=0;
+			int j=ch.length-1;
+			
+			int flag=0;
+			while(i<j)
+			{
+				if(ch[i]!=ch[j])
+				{
+					flag=1;
+					break;
+				}
+				i++;
+				j--;
+			}
+			if(flag==0)
+			{
+				System.out.println("Palindrome");
+			}
+			else
+			{
+				System.out.println("Not Palindrome");
+			}
+		
+	}
+}
